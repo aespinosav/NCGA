ebbf2dbb33db03e01d96ab6d8c452a7252f0425a
Alonso Espinosa <alonso.espinosa@aalto.fi>
2021-07-05 09:29:13 +0300

    Tidying code

--- Scratch/penalty.jl
+++ Scratch/penalty.jl
@@ -10,19 +10,19 @@ prohibited for the UE vehicles
                                     r_array)` 
 """
 function me_excluded_assignment_penalty(rn, ods, demands, γ, bounded_indices, r_array; solver=:gurobi) 
-    
+
     n = nv(rn.g)
     m = ne(rn.g)
-    
+
     # Cost structure (standard)
     #A = my_incidence_matrix(rn.g)
     A = incidence_matrix(rn.g)
     a = rn.edge_params[:a]
     B = Diagonal(rn.edge_params[:b])
-    
+
     # Vectors for conservation constraints (including OD terms)
     n_ods = length(ods)
-    
+
     d_vects_hv = SparseVector{Float64,Int64}[]
     d_vects_av = SparseVector{Float64,Int64}[]
     for i in 1:n_ods
@@ -30,29 +30,29 @@ function me_excluded_assignment_penalty(rn, ods, demands, γ, bounded_indices, r
         d_vec = spzeros(n)
         d_vec[s] = -demands[i]
         d_vec[d] = demands[i]
-        
+
         push!(d_vects_hv, (1-γ).*d_vec)
         push!(d_vects_av, γ.*d_vec)
     end
-    
+
     # ME objective function (unpenalised)       
     me_obj_func(x,y) = (0.5*(x+y)'*B*(x+y) + a'*x + 0.5*a'*y)[1]
-                             
+
     # Penalty function per link
     penalty_function(x, r, m) = r*(x^m)
-    
+
     # penalty term for objective
     function pen_term(x, param, inds; power=2)
        ps = penalty_function.(x[inds], 0.5*a[inds], power)
        param*sum(ps)
     end
-    
+
     # Penalised objective
     p_obj(x, y, p_param, b_indices) = me_obj_func(x,y) + 
                                       pen_term(x, p_param, b_indices)
 
     ### JuMP model construction
-    
+
     #p_stap = Model(Ipopt.Optimizer)
     #p_stap = Model(Gurobi.Optimizer)
     if solver == :gurobi
@@ -60,8 +60,8 @@ function me_excluded_assignment_penalty(rn, ods, demands, γ, bounded_indices, r
     elseif solver == :ipopt
         p_stap = Model(Ipopt.Optimizer)
     end
-    
-    # OD specific link flows HV         
+
+    # OD specific link flows HV
     @variable(p_stap, x[1:m,1:n_ods])
     @constraint(p_stap, non_neg_x, x .>= 0)
 
@@ -84,11 +84,11 @@ function me_excluded_assignment_penalty(rn, ods, demands, γ, bounded_indices, r
 
     # Unpenalised objective
     @objective(p_stap, Min, me_obj_func(x, y))
-    
-    ### 
+
+    ###
     ### Solve unpenalised
     ###
-    
+
     optimize!(p_stap)
 
     # Update
@@ -97,33 +97,33 @@ function me_excluded_assignment_penalty(rn, ods, demands, γ, bounded_indices, r
 
     iters_x = [sols_x[:]]
     iters_y = [sols_y[:]]
-    
+
     ###
     ### Sequence of penalised sub-problems
     ###
-    
+
     #r_array = [10, 100, 1000, 10000, 100000, 1000000]
-    
+
     for r in r_array
         @objective(p_stap,
                    Min,
                    p_obj(x, y, r, bounded_indices))
-        
+
         #for multiple ods another loop has to be added
         for k in 1:m
             set_start_value(x[k,1], sols_x[k])
             set_start_value(y[k,1], sols_y[k])
-        end           
-        
+        end
+
         optimize!(p_stap)
-        
+
         sols_x = value.(x)
         sols_y = value.(y)
 
         push!(iters_x, sols_x[:])
         push!(iters_y, sols_y[:])
     end
-    
+
     return iters_x, iters_y
     # Think about returning dual variables
 end
@@ -133,7 +133,7 @@ Run penalty method for network with restricted flow on some
 links for a range of penetration rates
 """
 function me_excluded_assignment_penalty_pr(rn, ods, demands, γ_array, bounded_indices, r_array; solver=:gurobi)
-    
+
     xx = []
     yy = []
     agg_a = []
@@ -149,7 +149,6 @@ function me_excluded_assignment_penalty_pr(rn, ods, demands, γ_array, bounded_i
         push!(yy, y)
         push!(agg_a, x+y)
     end
-    
+
     return xx, yy, agg_a
 end
-
