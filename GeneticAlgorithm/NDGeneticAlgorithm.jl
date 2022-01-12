5e814953d83a21a7f97d1c03c509b027ad5cbb7b
Alonso Espinosa <alonso.espinosa@aalto.fi>
2022-01-12 19:50:08 +0200

    Added crossover and mutation functions. Also tests have been added

--- GeneticAlgorithm/NDGeneticAlgorithm.jl
+++ GeneticAlgorithm/NDGeneticAlgorithm.jl
@@ -3,12 +3,22 @@
 
 module NDGeneticAlgorithm
 
-export Individual, fitness, crossover, mutate
+using Distributions
 
+import Base.==,
+       Base.length
+
+export Individual,
+       length,
+       fitness,
+       crossover,
+       mutation!
 
 ###
-### Definitions
+### Types and associated functions
 ###
+
+# Individuals
 
 """
 Individual in the genome population.
@@ -21,31 +31,95 @@ mutable struct Individual
     fitness::Float64
 end
 Individual(N::Int) = Individual(BitArray(undef, N), -Inf)
+Individual(G::BitArray) = Individual(G, -Inf)
 
+function ==(a::Individual, b::Individual)
+    a.genome == b.genome &&
+    a.fitness == b.fitness
+end
+
+length(a::Individual) = length(a.genome)
+
+
+# Population: for now to be treated as an array/set of individuals
+
+
+###
+### Functions
+###
 
 """
 Calculates fitness of individual
 """
-function fitness(indi::Individual, params)
+function fitness(a::Individual, params)
 
 end
 
-###
+
 ### Naive genetic operations
-###
 
 """
-Carries out crossover
+Carries out single-point crossover.
+If crossover point is provided, do deterministically.
 """
-function crossover()
+function crossover(a::Individual, b::Individual; x_point=nothing)
 
+    A = a.genome
+    B = b.genome
+
+    len = length(A)
+
+    ng1 = BitArray(undef, len)
+    ng2 = BitArray(undef, len)
+
+    if x_point == nothing
+        x_point = rand(2:len-1)
+    end
+
+    ng1[1:x_point] = A[1:x_point]
+    ng1[x_point:end] = B[x_point:end]
+
+    ng2[1:x_point] = B[1:x_point]
+    ng2[x_point:end] = A[x_point:end]
+
+    return Individual(ng1), Individual(ng2)
 end
 
 """
-Mutate offspring genome
+Draws random bernoulli variable vector
+for mutate! function
 """
-function mutate()
-
+function draw_bernoulli_vec(N::Int, p::Float64)
+    dist = Binomial(1, p)
+    vec = BitArray(rand(dist, N))
 end
 
+"""
+Mutate genome according to `mutation vector`
+"""
+function mutate!(a::Individual, mutation_vect::BitArray)
+    N = length(a.genome)
+    for i in 1:N
+        if mutation_vect[i] == 1
+            # Filp bit in genome
+            a.genome[i] = !(a.genome[i])
+        end
+    end
+end
+
+"""
+Function that mutates an individual's (`a`) genome
+according to probability `p`.
+
+Calls mutate! after generating the mutation vector
+and passing it as an argument.
+"""
+function mutation!(a::Individual, p)
+    N = length(a.genome)
+    mv = draw_bernoulli_vec(N, p)
+    mutate!(a, mv)
+end
+
+
+# Module end
 end
---
+++ GeneticAlgorithm/module_tests.jl
@@ -0,0 +1,42 @@
+# Tests for NDGeneticAlgorithm module
+
+using NDGeneticAlgorithm,
+      Test
+
+@testset "General module tests" begin
+
+    @testset "Tests for `Individual` struct" begin
+        ### Test `Individual` instantiation
+        a = Individual(5)
+        b = Individual(BitArray(zeros(5)), -Inf)
+
+        # Equality of variables
+        @test a.genome == b.genome
+        @test a.fitness == b.fitness
+
+        # Equality of `Individual`s
+        @test a == b
+    end
+
+    @testset "Tests for `crossover`" begin
+        c = Individual(BitArray([0,0,0,1,1,1]))
+        d = Individual(BitArray([1,1,1,0,0,0]))
+        off1, off2 = crossover(c, d, x_point=4)
+
+        @test off1.genome == BitArray(zeros(6))
+        @test off2.genome == BitArray(ones(6))
+    end
+
+    @testset "Tests for mutation!" begin
+        a = Individual(5)
+                NDGeneticAlgorithm.mutate!(a, BitArray([1,0,0,0,0]))
+
+        @test a.genome == BitArray([1,0,0,0,0])
+
+        b = Individual(5)
+        mutation!(b, 0.7)
+
+        @test typeof(b) == Individual
+        @test typeof(b.genome) == BitArray{1}
+    end
+end
