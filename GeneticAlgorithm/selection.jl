64f75858945984fa5ecd288ff800052614e9a44b
Alonso Espinosa <alonso.espinosa@aalto.fi>
2022-01-27 12:21:32 +0200

    Added tournament selection

--- GeneticAlgorithm/selection.jl
+++ GeneticAlgorithm/selection.jl
@@ -16,12 +16,20 @@ function fitprop_selection_prob(pop::Array{Individual,1})
 end
 
 """
-Select single individual randomly proportional to fitness
+Select single individual randomly proportional to fitness.
+
+This is inefficient since it calculates probabilities of 
+all individiuals in pop to select only 1...
 """
 function select_individual(pop::Array{Individual,1})
     weights = fitprop_selection_prob(pop)
     sample(pop, weights)
 end
+
+function select_individual(pop::Array{Individual,1}, w::AbstractWeights)
+    sample(pop, w)
+end
+
 
 """
 Select (returns array) `k` individuals, selected randomly
@@ -38,8 +46,24 @@ end
 
 ### Tournament (ordinal selection)
 
+"""
+Select mating individual from a `q`-tournament
+"""
 function tournament_selection(pop::Array{Individual,1}, q)
     contestants = rand(pop, q)
-    sort!(contestants, by= x->x.fitness, rev=true)
+    sort!(contestants, by=x->x.fitness, rev=true)
     contestants[1]
+end
+
+"""
+Select (returns array) `k` individuals, selected via tournamets
+of `q` participants.
+"""
+function select_mating_array(pop::Array{Individual,1}, k::Int, q::Int)
+    mating_array = Individual[]
+    for i in 1:k
+        victor = tournament_selection(pop, q)
+        push!(mating_array, victor)
+    end
+    mating_array
 end
