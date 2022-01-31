b596df85cbd2a36d571467d52fc00ea5e9b71d11
Alonso Espinosa <alonso.espinosa@aalto.fi>
2022-01-31 10:36:57 +0200

    We now have unicode plotting of networks in the terminalgit diff plotting.jl

--- GeneticAlgorithm/plotting.jl
+++ GeneticAlgorithm/plotting.jl
@@ -34,3 +34,45 @@ function plot_evolution(gens; stride=1)
     end
     p
 end
+
+"""
+Plotting in terminal of network with highlighting of links
+for AV exclusive use.
+
+Based on `term_plot` in TrafficNetworks2
+"""
+function term_link_plot(rn::RoadNetwork, a::Individual, map_dict::Dict{Int64,Int64})
+    node_positions = rn.node_params[:pos]
+    edge_num = ne(rn.g)
+
+    genome_ones = findall(x->x==1, a.genome)
+    av_edges = [map_dict[i] for i in genome_ones]
+
+    edge_coords = Array{NTuple{4,Float64},1}(undef, edge_num)
+    for (i, ed) in enumerate(edges(rn.g))
+        x_src, y_src = node_positions[ed.src,:]
+        x_dst, y_dst = node_positions[ed.dst,:]
+        edge_coords[i] = (x_src, y_src, x_dst, y_dst)
+    end
+
+    canvas = BrailleCanvas(60,
+                           25,
+                           origin_x = 0.0,
+                           origin_y = 0.0,
+                           width = 1.0,
+                           height = 1.0)
+    for i in 1:edge_num
+        if i ∉ av_edges
+            lines!(canvas, edge_coords[i]...)
+        end
+
+    end
+
+    for i in 1:edge_num
+        if i in av_edges
+            lines!(canvas, edge_coords[i]..., color=:red)
+        end
+    end
+
+    canvas
+end
